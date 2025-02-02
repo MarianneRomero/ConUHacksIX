ff1fc0e94229c792ce0704d6c7cc5c1634a84320
Lucia-Cai <cai.lucia04@gmail.com>
2025-02-02 01:08:31 -0500

    created graph branch

--- src/frontend/src/mood.js
+++ src/frontend/src/mood.js
@@ -29,38 +29,43 @@ const fake_data = [
     }
   ];
 
-
-const MoodTracker = ({ fake_data }) => {
-  return (
-    <RadialBarChart
-      width={730}
-      height={250}
-      innerRadius="10%"
-      outerRadius="80%"
-      data={data}
-      startAngle={180}
-      endAngle={0}
-    >
-      <RadialBar
-        minAngle={15}
-        label={{ fill: '#666', position: 'insideStart' }}
-        background
-        clockWise={true}
-        dataKey="uv"
-      />
-      <Legend
-        iconSize={10}
-        width={120}
-        height={140}
-        layout="vertical"
-        verticalAlign="middle"
-        align="right"
-      />
-      <Tooltip />
-    </RadialBarChart>
-  );
-};
-
-export default MoodRadialChart;
-
-
+function MoodTracker() {
+    
+    return (
+        <div>
+            <div>
+                <h1>Mood Tracker</h1>
+            </div>
+            <div>
+                <RadialBarChart
+                    width={730}
+                    height={250}
+                    innerRadius="10%"
+                    outerRadius="80%"
+                    data={data}
+                    startAngle={180}
+                    endAngle={0}
+                >
+                    <RadialBar
+                    minAngle={15}
+                    label={{ fill: '#666', position: 'insideStart' }}
+                    background
+                    clockWise={true}
+                    dataKey="uv"
+                    />
+                    <Legend
+                    iconSize={10}
+                    width={120}
+                    height={140}
+                    layout="vertical"
+                    verticalAlign="middle"
+                    align="right"
+                    />
+                    <Tooltip />
+                </RadialBarChart>
+            </div>
+        </div>
+    );
+  };
+  
+  export default MoodChart;
